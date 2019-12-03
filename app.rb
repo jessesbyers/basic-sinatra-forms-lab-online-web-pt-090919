3a02e6950d3caa3b4588210f81a304a3e98bffa2
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-03 14:03:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  # Create a route that responds to a POST request at /team
+  # Have the form send a POST request to this route.
+  # Upon submission, pass the submitted data to the team.erb template.
+  post "/team" do
+    puts params
+    erb :team
+  end
+
+
 
 end
